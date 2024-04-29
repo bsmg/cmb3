5f3fc814bc41d29b930c7363ee0af709353d7845
Riley Prince <NuggoDEV@gmail.com>
2024-04-29 20:04:30 +0930

    Register commands, event for interactionCreate

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Configuration } from "./config";
-import { client } from "./client";
+import { client, commands } from "./client";
+import { registerCommands } from "./commands";
 
 Configuration.Init();
 
+registerCommands();
+
 client.on("ready", (client) => {
     console.log(`Logged into ${client.user.username} (${client.user.id})`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand())
+        return;
+
+    const command = commands.get(interaction.commandName);
+
+    if (!command)
+        throw new Error("Command not found for: " + interaction.commandName);
+
+    await command();
 });
 
 client.login(Configuration.config.token);
