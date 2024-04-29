53571e2aec2b8008008ee8b603a923996357e0df
Riley Prince <NuggoDEV@gmail.com>
2024-04-29 20:05:05 +0930

    Register commands with REST

---
+++ src/commands.ts
@@ -0,0 +1,24 @@
+import { commands as clientCommands } from "./client";
+
+import { REST } from "@discordjs/rest";
+import { readdirSync } from "fs";
+import { join } from "path";
+import { Configuration } from "./config";
+import { Routes } from "discord.js";
+
+export async function registerCommands()
+{
+	const rest = new REST().setToken(Configuration.config.token);
+
+	const commandFiles = readdirSync(join(__dirname, "commands")).filter(f => f.endsWith(".ts"));
+	const commands = [];
+
+	for (const file of commandFiles)
+	{
+		const { command, execute } = require(`./commands/${file}`);
+		commands.push(command);
+		clientCommands.set(command.name, execute);
+	}
+	
+	await rest.put(Routes.applicationCommands(Configuration.config.clientId), { body: commands });
+}
