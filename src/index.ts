cecb369fc5453f845f83bc0b0ff2d7a624d1886a
Riley Prince <NuggoDEV@gmail.com>
2024-04-30 22:42:37 +0930

    Register events, move existing events into own files

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,11 @@
 import { Configuration } from "./config";
 import { client, commands } from "./client";
 import { registerCommands } from "./commands";
+import { registerEvents } from "./events";
 
 Configuration.Init();
 
 registerCommands();
-
-client.on("ready", (client) => {
-    console.log(`Logged into ${client.user.username} (${client.user.id})`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isChatInputCommand())
-        return;
-
-    const command = commands.get(interaction.commandName);
-
-    if (!command)
-        throw new Error("Command not found for: " + interaction.commandName);
-
-    await command(interaction);
-});
+registerEvents();
 
 client.login(Configuration.config.token);
