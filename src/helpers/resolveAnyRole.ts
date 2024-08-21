aeda416ba2b4dc28065b00be3294cda7d34d93b9
Riley Prince <NuggoDEV@gmail.com>
2024-08-21 23:49:37 +0930

    remove useless helper

--- src/helpers/resolveAnyRole.ts
+++
@@ -1,8 +0,0 @@
-import type { GuildMember } from "discord.js";
-
-export function resolveAnyRole(
-  member: GuildMember,
-  roleIds: string[],
-): boolean {
-  return member.roles.cache.hasAny(...roleIds);
-}
