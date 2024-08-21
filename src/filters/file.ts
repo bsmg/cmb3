ad7ef0467a502049c5ded50a6862c1f2f69d09a3
Riley Prince <NuggoDEV@gmail.com>
2024-08-21 23:46:59 +0930

    Rename fileFilter, change priority

--- src/filters/file.ts
+++ src/filters/file.ts
@@ -2,7 +2,7 @@ import type { Message } from "discord.js";
 import type { IFilter } from "../interfaces/filter";
 
 export default class FileFilter implements IFilter {
-  public priority = 0;
+  public priority = 5;
   public name = "FileFilter";
 
   public async execute(_message: Message<boolean>) {}
