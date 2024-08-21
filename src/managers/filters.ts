0a1a25ed25ebd6da36a35d1dec882cc5ccf937e1
Riley Prince <NuggoDEV@gmail.com>
2024-08-21 23:50:32 +0930

    Filter registration & execution. Reverse priority, 0 goes first

--- src/managers/filters.ts
+++ src/managers/filters.ts
@@ -1,16 +1,19 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
 import type { Message } from "discord.js";
 import type { IFilter } from "../interfaces/filter";
 
 export class FilterManager {
   private static _instance: FilterManager;
   private _filters: IFilter[] = [];
+  private readonly _filtersPath = path.join(__dirname, "..", "filters");
 
   public static get instance() {
     if (!this._instance) this._instance = new FilterManager();
     return this._instance;
   }
 
-  public registerFilter(filter: IFilter) {
+  private registerFilter(filter: IFilter) {
     console.log(`Registering filter "${filter.name}"`);
 
     if (this._filters.some((x) => x.name === filter.name)) {
@@ -18,11 +21,28 @@ export class FilterManager {
     }
 
     this._filters.push(filter);
-    this._filters.sort((a, b) => b.priority - a.priority);
+    this._filters.sort((a, b) => a.priority - b.priority);
   }
 
   public async setupFilters() {
     this._filters = [];
+
+    let files = await fs.readdir(this._filtersPath, {
+      withFileTypes: true,
+    });
+
+    files = files.filter((x) => x.isFile() && x.name.endsWith(".ts"));
+
+    const filePromises = files.map(async (file) => {
+      const module = await import(path.join(file.parentPath, file.name));
+
+      if (module.default) {
+        const instance = new module.default() as IFilter;
+        this.registerFilter(instance);
+      }
+    });
+
+    await Promise.all(filePromises);
   }
 
   public async runFilters(message: Message<boolean>) {
