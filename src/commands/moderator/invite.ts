329a33d6fb5df6fdc902baa6bc74f452d6d97bfe
Riley Prince <NuggoDEV@gmail.com>
2024-08-22 13:35:13 +0930

    implement guild info getter for invite code

--- src/commands/moderator/invite.ts
+++ src/commands/moderator/invite.ts
@@ -1,5 +1,6 @@
 /* eslint-disable unicorn/consistent-function-scoping */
 import * as process from "node:process";
+import { EmbedBuilder } from "@discordjs/builders";
 import type {
   CacheType,
   CommandInteraction,
@@ -18,22 +19,33 @@ export default class Invite implements ICommand {
     .addSubcommand((subcommand) =>
       subcommand
         .setName("add")
-        .setDescription("Add a server id to the whitelist.")
+        .setDescription("Add a guild id to the whitelist.")
         .addStringOption((option) =>
           option
             .setName("id")
-            .setDescription("The server id to be added to the whitelist.")
+            .setDescription("The guild id to be added to the whitelist.")
             .setRequired(true),
         ),
     )
     .addSubcommand((subcommand) =>
       subcommand
         .setName("remove")
-        .setDescription("Remove a server id from the whitelist.")
+        .setDescription("Remove a guild id from the whitelist.")
         .addStringOption((option) =>
           option
             .setName("id")
-            .setDescription("The server id to be removed from the whitelist.")
+            .setDescription("The guild id to be removed from the whitelist.")
+            .setRequired(true),
+        ),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("get")
+        .setDescription("Gets a guild's id from an invite url")
+        .addStringOption((option) =>
+          option
+            .setName("code")
+            .setDescription("The guild invite code to get the id from.")
             .setRequired(true),
         ),
     );
@@ -48,8 +60,70 @@ export default class Invite implements ICommand {
     const options = interaction.options as CommandInteractionOptionResolver;
 
     switch (options.getSubcommand()) {
-      case "add":
-      // PostgresManager.instance.pool.query(``)
+      case "get":
+        await this.handleGetInvite(interaction, options);
+        break;
+      // id ? interaction.reply(`Guild found!`)
     }
+  }
+
+  private async handleGetInvite(
+    interaction: CommandInteraction<CacheType>,
+    options: CommandInteractionOptionResolver,
+  ) {
+    const code = options.getString("code") as string;
+    const guildInfo = await this.getGuildFromCode(code);
+
+    if (!guildInfo.guild) {
+      await interaction.reply({
+        content: `Failed to get guild info for \`${code}\``,
+        ephemeral: true,
+      });
+
+      return;
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Invite Found: ${guildInfo.guild.name}`)
+      .setColor(0x00ff00)
+      .addFields(
+        {
+          name: "Invite",
+          value: `https://discord.gg/${code}`,
+          inline: false
+        },
+        {
+          name: "Expires",
+          value: guildInfo.expires_at ? `<t:${Date.parse(guildInfo.expires_at) / 1_000}>` : "Never",
+          inline: false
+        },
+        { 
+          name: "ID", 
+          value: guildInfo.guild.id, 
+          inline: false 
+        },
+        {
+          name: "Description",
+          value: guildInfo.guild.description ?? "No description",
+          inline: false,
+        },
+        {
+          name: "Members",
+          value: Number(guildInfo.approximate_member_count).toLocaleString(),
+          inline: false
+        }
+      )
+      .setThumbnail(
+        Constants.guildIcon(guildInfo.guild_id, guildInfo.guild.icon),
+      );
+
+    await interaction.reply({
+      embeds: [embed],
+    });
+  }
+
+  private async getGuildFromCode(code: string) {
+    const inviteData = await fetch(Constants.guildInvite(code));
+    return JSON.parse(await inviteData.text());
   }
 }
