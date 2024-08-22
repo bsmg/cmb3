d31cfb186921d72f60d199774fc43568c7b1aa94
Riley Prince <NuggoDEV@gmail.com>
2024-08-22 13:34:52 +0930

    temporarily disable postgres manager

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { ClientManager } from "./managers/client";
 import { CommandManager } from "./managers/commands";
 import { EventManager } from "./managers/events";
 import { FilterManager } from "./managers/filters";
-import { PostgresManager } from "./managers/postgres";
+// import { PostgresManager } from "./managers/postgres";
 
 (async () => {
   dotenv.config();
 
-  await PostgresManager.instance.setup();
+  // await PostgresManager.instance.setup();
   await EventManager.instance.setupEvents();
   await CommandManager.instance.loadCommands();
   await FilterManager.instance.setupFilters();
