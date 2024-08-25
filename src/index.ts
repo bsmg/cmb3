3c37ff20f914a31fe7ab0959a5eae1e8b2333bad
Riley Prince <nuggodev@gmail.com>
2024-08-26 00:02:25 +0930

    Remove mentions of pg

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,13 @@ import { ClientManager } from "./managers/client";
 import { CommandManager } from "./managers/commands";
 import { EventManager } from "./managers/events";
 import { FilterManager } from "./managers/filters";
-// import { PostgresManager } from "./managers/postgres";
 
 (async () => {
   dotenv.config();
 
-  // await PostgresManager.instance.setup();
   await EventManager.instance.setupEvents();
   await CommandManager.instance.loadCommands();
   await FilterManager.instance.setupFilters();
 
-  await ClientManager.instance.client.login(process.env.Token);
+  await ClientManager.client.login(process.env.Token);
 })();
