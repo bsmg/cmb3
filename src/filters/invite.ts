81514847e057d26ce69c6d5d2ae3da95ca0f4bdf
Riley Prince <nuggodev@gmail.com>
2024-08-26 12:47:23 +0930

    Simple implementation of invite filter, must implement role detection, admin+ not affected by filter

--- src/filters/invite.ts
+++ src/filters/invite.ts
@@ -1,10 +1,43 @@
 import type { Message } from "discord.js";
+import { getGuildFromCode } from "../helpers/invite";
 import type { IFilter } from "../interfaces/filter";
-// import { PrismaManager } from "../managers/prisma";
+import { PrismaManager } from "../managers/prisma";
 
 export default class InviteFilter implements IFilter {
-  public priority = 0;
-  public name = "InviteFilter";
+  public readonly priority = 0;
+  public readonly name = "InviteFilter";
+  private readonly regex = /discord\.(?:gg|com\/invite)\/[\w-]+/gi;
 
-  public async execute(_message: Message<boolean>) {}
+  public async execute(message: Message<boolean>) {
+    const regexMatches = this.regex.exec(message.content);
+
+    if (regexMatches === null) return;
+
+    const results = regexMatches.map(async (value) => {
+      const code = value.slice(value.lastIndexOf("/") + 1);
+
+      const guildInfo = await getGuildFromCode(code);
+
+      if (!guildInfo) {
+        console.log("deleting message!");
+        await message.delete();
+        // add logging to channel
+        return;
+      }
+
+      const guild = await PrismaManager.client.guildWhitelist.findUnique({
+        where: {
+          id: guildInfo.id,
+        },
+      });
+
+      if (!guild) {
+        console.log("deleting message late!");
+        await message.delete();
+        // log to channel
+      }
+    });
+
+    await Promise.all(results);
+  }
 }
